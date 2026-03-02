88c28539f2299ba116f909cd19f7568a27740b67
gosuni <gosuni2025@gmail.com>
2026-03-02 21:45:20 +0900

    Auto-save: 2026. 3. 2. 오후 9:45:20

--- js/plugins/CustomSceneEngine.js
+++ js/plugins/CustomSceneEngine.js
@@ -1404,12 +1404,43 @@
     var aw = navMgr._activeIndex >= 0 ? navMgr._focusables[navMgr._activeIndex] : null;
     var aid = aw ? aw._id : null;
     var isLinked = aid && linked.indexOf(aid) >= 0;
-    // Window.opacity setter는 _windowSpriteContainer.alpha만 변경하므로
-    // 자식 Sprite(이미지 등)에 cascade가 전달되지 않음.
-    // Window 자체의 alpha를 설정해야 모든 자식이 함께 dim됨.
     var dimAlpha = isLinked ? 1.0 : 0.63;
-    if (Math.abs((this._displayObject.alpha || 1) - dimAlpha) > 0.005) {
+    var prevAlpha = this._displayObject.alpha;
+    if (Math.abs((prevAlpha || 1) - dimAlpha) > 0.005) {
       this._displayObject.alpha = dimAlpha;
+      // --- DEBUG ---
+      var wsc = this._displayObject._windowSpriteContainer;
+      console.log('[DIM] panel=' + this._id +
+        ' isLinked=' + isLinked +
+        ' alpha: ' + prevAlpha + ' → ' + this._displayObject.alpha +
+        ' | wsc.alpha=' + (wsc ? wsc.alpha : 'N/A') +
+        ' | wsc.children=' + (wsc ? wsc.children.length : 'N/A'));
+      if (wsc) {
+        for (var _di = 0; _di < wsc.children.length; _di++) {
+          var _dc = wsc.children[_di];
+          console.log('  wsc.children[' + _di + '] alpha=' + _dc.alpha +
+            ' worldAlpha=' + (_dc.worldAlpha !== undefined ? _dc.worldAlpha : '?') +
+            ' visible=' + _dc.visible +
+            ' class=' + (_dc.constructor && _dc.constructor.name || '?'));
+        }
+      }
+      // 패널 자식 위젯 중 Widget_Image 찾아서 그 Sprite의 실제 parent 경로 출력
+      for (var _ci = 0; _ci < this._children.length; _ci++) {
+        var _cw = this._children[_ci];
+        var _co = _cw && _cw.displayObject && _cw.displayObject();
+        if (_co) {
+          var _path = [];
+          var _p = _co;
+          for (var _pi = 0; _pi < 6 && _p; _pi++) {
+            _path.unshift(_p.constructor && _p.constructor.name || '?');
+            _p = _p.parent;
+          }
+          console.log('  child[' + _cw._id + '] displayObj.alpha=' + _co.alpha +
+            ' worldAlpha=' + (_co.worldAlpha !== undefined ? _co.worldAlpha : '?') +
+            ' parent chain: ' + _path.join(' → '));
+        }
+      }
+      // --- END DEBUG ---
     }
   };
   Widget_Panel.prototype.destroy = function() {
@@ -4440,13 +4471,23 @@
       this._helpWindow.show();
     };
 
-    // selectEnemySelection: actorCommand 비활성화 + actorWindow dim 처리
+    // selectEnemySelection: actorCommand 비활성화 + 하단 패널 dim + enemyWindow 최상단으로
     var origSES = SCB.selectEnemySelection || function() {};
     Klass.prototype.selectEnemySelection = function() {
       var wmap = this._widgetMap || {};
       if (wmap.actorCommand && wmap.actorCommand.deactivate) wmap.actorCommand.deactivate();
-      var actorWidget = wmap['actorWindow'];
-      if (actorWidget && actorWidget._window) actorWidget._window.contentsOpacity = 80;
+      // actorWindow / statusWindow dim
+      ['actorWindow', 'statusWindow'].forEach(function(id) {
+        var w = wmap[id];
+        if (w && w._window) w._window.alpha = 0.35;
+      });
+      // enemyWindow를 windowLayer 맨 앞으로 (z-order)
+      var enemyWidget = wmap['enemyWindow'];
+      if (enemyWidget && enemyWidget._window && enemyWidget._window.parent) {
+        var layer = enemyWidget._window.parent;
+        layer.removeChild(enemyWidget._window);
+        layer.addChild(enemyWidget._window);
+      }
       origSES.call(this);
     };
 
@@ -4509,9 +4550,12 @@
 
     Klass.prototype.onEnemyCancel = function() {
       this._enemyWindow.hide();
-      // actorWindow dim 복구
-      var actorWidget = this._widgetMap && this._widgetMap['actorWindow'];
-      if (actorWidget && actorWidget._window) actorWidget._window.contentsOpacity = 255;
+      // dim 복구
+      var wmap = this._widgetMap || {};
+      ['actorWindow', 'statusWindow'].forEach(function(id) {
+        var w = wmap[id];
+        if (w && w._window) w._window.alpha = 1;
+      });
       var last = this._ctx.lastActorCommand || 'attack';
       if (last === 'attack') { this._actorCommandWindow.activate(); }
       else if (last === 'skill') { this._skillWindow.show(); this._skillWindow.activate(); }
